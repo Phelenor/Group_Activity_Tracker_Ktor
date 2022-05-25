7a49191f466ca6937674a84b12336a34c9df0680
Rafael Boban <rafael.boban00@gmail.com>
2022-05-25 15:41:13 +0200

    Update MessageDataSource

--- src/main/kotlin/com/rafaelboban/data/message/MessageDataSource.kt
+++ src/main/kotlin/com/rafaelboban/data/message/MessageDataSource.kt
@@ -1,13 +1,14 @@
 package com.rafaelboban.data.message
 
 import org.litote.kmongo.coroutine.CoroutineDatabase
+import org.litote.kmongo.eq
 
 class MessageDataSource(db: CoroutineDatabase) {
 
     private val messages = db.getCollection<Message>()
 
-    suspend fun getAllMessages(): List<Message> {
-        return messages.find()
+    suspend fun getMessagesForEvent(eventId: String): List<Message> {
+        return messages.find(Message::id eq eventId)
             .descendingSort(Message::timestamp)
             .toList()
     }
