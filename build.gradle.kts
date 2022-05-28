278735419a7fdd411622ac7de6e7b3bba9ea9e16
Rafael Boban <rafael.boban00@gmail.com>
2022-05-29 00:16:19 +0200

    Remove unused variable

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ val kotlin_version: String by project
 val logback_version: String by project
 val kmongo_version: String by project
 val koin_version: String by project
-val commons_codec_version: String by project
 
 plugins {
     application
