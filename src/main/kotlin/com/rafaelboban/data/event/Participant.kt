c2de53b7690b385256b4e497d6674be0c905129c
Rafael Boban <rafael.boban00@gmail.com>
2022-06-06 03:15:47 +0200

    Delete unused classes

--- src/main/kotlin/com/rafaelboban/data/event/Participant.kt
+++ src/main/kotlin/com/rafaelboban/data/event/Participant.kt
@@ -5,7 +5,5 @@ import io.ktor.websocket.*
 data class Participant(
     val id: String,
     val username: String,
-    var socket: WebSocketSession,
-    var distance: Double = 0.0,
-    var isActive: Boolean = true
+    var socket: WebSocketSession
 )
